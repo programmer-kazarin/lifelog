63be057726fffc92ac18848b1be2d2211d92a600
kazarin <alexander.kazarin@magenta-technology.com>
2020-06-06 23:07:48 +0300

    project.properties

--- src/main/java/com/kzrn/App.java
+++ src/main/java/com/kzrn/App.java
@@ -1,10 +1,19 @@
 package com.kzrn;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Properties properties = new Properties();
+        try (InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("app.properties")) {
+            properties.load(input);
+            System.out.println(properties.getProperty("token"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
